8eaf20ced8650d52ddb170991dfb1f6a546099c9
Hiba442 <hibaalrmileh@gmail.com>
2022-08-24 14:36:52 +0200

    Aufgabe 1 erledigt.

--- 1_UrbungVerzweigung/Program.cs
+++ 1_UrbungVerzweigung/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int x = 9, z = 6; double y = 8;
+Console.WriteLine(x-- == y);
+Console.WriteLine(x == (int)y);
+Console.WriteLine("{0:F1}", 0,123);
+Console.WriteLine(z/y);
+Console.WriteLine(x -= 10);
+Console.WriteLine(x-- == y & true);
+Console.WriteLine(!(z == 6) | false);
+Console.WriteLine((x *= -2) == z);
